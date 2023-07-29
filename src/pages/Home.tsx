49e8207773cb65b7cc6006f4b3643a91e7cfa599
aravind bairwa <aravind@furthr.earth>
2023-07-29 15:43:04 +0530

    feat: added styles and animations on the home page as theme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import Table from "../components/Table";
 
 const Home: React.FC = () => {
-  return <div className='text-3xl font-bold underline'>Crickets List</div>;
+  return (
+    
+      <section>
+        <div className="flex flex-col justify-center items-center p-20 h-[100vh]">
+      <Table />
+    </div>
+
+  <div className='air air1'></div>
+  <div className='air air2'></div>
+  <div className='air air3'></div>
+  <div className='air air4'></div>
+</section>
+  );
 };
 
 export default Home;
