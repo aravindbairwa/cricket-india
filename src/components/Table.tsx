1baaef4df24ade48371f92e2db3334b947e24ec3
aravind bairwa <aravind@furthr.earth>
2023-07-31 04:01:12 +0530

    imp: Added logic to handle filters and sorting in the table component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useCallback, useEffect, useState } from "react";
 import Paper from "@mui/material/Paper";
 import { getAgeFromTimestamp } from "../utils";
 import getPlayers, { TPlayer } from "../api-data/get-players";
@@ -17,7 +17,17 @@ const columns: GridColDef[] = [
     headerName: "Name",
     minWidth: 300,
     renderCell: (params: GridRenderCellParams<String>) => (
-      <Link to={`/${params.value}`}>{params.value}</Link>
+      <Link
+        to={`/${params.value}`}
+        onClick={() =>
+          localStorage.setItem(
+            "activeSortingAndFilters",
+            JSON.stringify(activeSortingAndFilters)
+          )
+        }
+      >
+        {params.value}
+      </Link>
     ),
     valueGetter: (params: GridValueGetterParams) => `${params.row.name || ""}`,
   },
@@ -40,44 +50,125 @@ const columns: GridColDef[] = [
 ];
 
 let rows: Player[] = [];
+let tableInitialState: Object = {
+  pagination: {
+    paginationModel: { page: 0, pageSize: 10 },
+  },
+};
+let activeSortingAndFilters = {
+  sorting: {
+    sortModel: [
+      {
+        field: "name",
+        sort: "asc",
+      },
+    ],
+  },
+  filter: {
+    filterModel: {
+      items: [
+        {
+          field: "name",
+          operator: "contains",
+          value: "",
+          id: 1,
+        },
+      ],
+    },
+  },
+};
 
 export default function StickyHeadTable() {
   const [, setCricketers] = useState<TPlayer[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    const getAllCricketers = async () => {
-      try {
-        const fetchedData = await getPlayers();
-        setCricketers(fetchedData);
 
-        let a = fetchedData;
-        let b = JSON.parse(JSON.stringify(a));
-        rows = b.map((cricketer: Player) => {
-          const cricketersAge = getAgeFromTimestamp(cricketer.dob);
-          return {
-            ...cricketer,
-            age: cricketersAge,
-          };
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  const getAllCricketers = useCallback(async () => {
+    try {
+      setLoading(true);
+      const fetchedData = await getPlayers();
+      setCricketers(fetchedData);
 
-    getAllCricketers();
+      let a = fetchedData;
+      let b = JSON.parse(JSON.stringify(a));
+      rows = b.map((cricketer: Player) => {
+        const cricketersAge = getAgeFromTimestamp(cricketer.dob);
+        return {
+          ...cricketer,
+          age: cricketersAge,
+        };
+      });
+      const isValue = localStorage.getItem("activeSortingAndFilters");
+      if (isValue) activeSortingAndFilters = JSON.parse(isValue);
+      tableInitialState = { ...tableInitialState, ...activeSortingAndFilters };
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
   }, []);
 
+  useEffect(() => {
+    getAllCricketers();
+  }, [getAllCricketers]);
+
+  const handleSortingAndFilters = useCallback(
+    (newState: any, changeType: string) => {
+      console.log(newState);
+
+      if(newState[0] === null || newState[0] === undefined) return;
+      if (changeType === "sort") {
+        const isSortApplied = activeSortingAndFilters.sorting.sortModel.some(
+          (s) => {
+            return s?.field.toLowerCase() === newState[0]?.field.toLowerCase();
+          }
+        );
+
+        if (!isSortApplied) {
+          activeSortingAndFilters.sorting.sortModel.unshift(newState[0]);
+        } else {
+          activeSortingAndFilters.sorting.sortModel.forEach((s) => {
+            if (s.field === newState[0].field) s.sort = newState[0].sort;
+          });
+        }
+      } else {
+        const isFilterApplied =
+          activeSortingAndFilters.filter.filterModel.items.some((f) => {
+            return f?.field.toLowerCase() === newState[0]?.field.toLowerCase();
+          });
+        if (!isFilterApplied) {
+          activeSortingAndFilters.filter.filterModel.items.unshift(newState[0]);
+        } else {
+          activeSortingAndFilters.filter.filterModel.items.forEach((f) => {
+            if (f.field === newState[0].field) {
+              f.operator = newState[0].operator;
+              f.value = newState[0].value;
+              f.id = newState[0].id;
+            }
+          });
+        }
+      }
+
+      localStorage.setItem(
+        "activeSortingAndFilters",
+        JSON.stringify(activeSortingAndFilters)
+      );
+    },
+    []
+  );
+
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+    <Paper sx={{ width: "100%", overflow: "hidden", zIndex: 5 }}>
       <DataGrid
+        loading={loading}
         rows={rows}
         columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
+        initialState={tableInitialState}
+        pageSizeOptions={[10, 20]}
+        sortingOrder={["asc", "desc"]}
+        onFilterModelChange={(filter) => {
+          handleSortingAndFilters(filter.items, "filter");
         }}
-        pageSizeOptions={[5, 10]}
+        onSortModelChange={(sort) => handleSortingAndFilters(sort, "sort")}
       />
     </Paper>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CricketIndia from "../components/CricketIndia";
 import Table from "../components/Table";
 
 const Home: React.FC = () => {
-  return (
-    <section>
-      <div className="flex flex-col justify-center items-center p-20 h-[100vh]">
-        <Table />
-      </div>
+  const [showAnimation, setShowAnimation] = useState<boolean>(true);
 
-      <div className="air air1"></div>
-      <div className="air air2"></div>
-      <div className="air air3"></div>
-      <div className="air air4"></div>
-    </section>
+  useEffect(() => {
+    setTimeout(() => setShowAnimation(false), 4200);
+  }, [showAnimation]);
+
+  return (
+    <div className="relative">
+      {showAnimation ? (
+        <CricketIndia />
+       ) : ( 
+        <section>
+          <div className="flex flex-col justify-center items-center p-20 h-[100vh]">
+            <Table />
+          </div>
+
+          <div className="air air1"></div>
+          <div className="air air2"></div>
+          <div className="air air3"></div>
+          <div className="air air4"></div>
+        </section>
+      )}
+    </div>
   );
 };
 
